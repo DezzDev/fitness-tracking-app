a1d9869aa6e25f8eb2af817843bc15c96ed68ed0
Daniel Z <daniel-dezz@hotmail.com>
2026-02-27 15:53:17 +0100

    start new design of frontend

--- frontend/src/components/layouts/DashboardLayout.tsx
+++ frontend/src/components/layouts/DashboardLayout.tsx
@@ -7,7 +7,8 @@ import {
 	User,
 	LogOut,
 	Menu,
-	LucideSidebarClose
+	LucideSidebarClose,
+	
 } from "lucide-react";
 import { useState } from 'react';
 import { useAuthStore } from "@/store/authStore";
@@ -40,7 +41,7 @@ function DashboardLayout() {
 	];
 
 	return (
-		<div className="min-h-screen bg-gray-50">
+		<div className="bg-background text-foreground">
 			{/* Header */}
 			<header className="bg-white border-b border-gray-200 sticky top-0 z-40">
 				<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
--- frontend/src/features/dashboard/pages/DashboardPage.tsx
+++ frontend/src/features/dashboard/pages/DashboardPage.tsx
@@ -1,8 +1,51 @@
 
 
 function DashboardPage() {
+
+
+	const renderDashboard = () => {
+		switch (screen) {
+			case "entry":
+				return (
+					<EntryScreen
+						session={SESSION}
+						onStart={handleStart}
+						completed={false}
+					/>
+				);
+			case "active":
+				return (
+					<ActiveSession session={SESSION} onComplete={handleComplete} />
+				);
+			case "complete":
+				return (
+					<CompletionScreen
+						session={SESSION}
+						completedSets={completedSets}
+						onReturn={handleReturn}
+					/>
+				);
+			case "entry-done":
+				return (
+					<EntryScreen
+						session={SESSION}
+						onStart={handleStart}
+						completed={true}
+					/>
+				);
+			default:
+				return null;
+		}
+	};
+
 	return (
-		<div>DashboardPage</div>
+		<div className="h-full w-full overflow-hidden font-barlow">
+			{/* Main context */}
+			<div className="flex-1 overflow-hidden position-relative bg-background">
+			hola
+				{renderDashboard()}
+			</div>
+		</div>
 	)
 }
 
